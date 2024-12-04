a300f1a43d612bdf8acf6223e62d9a94e85c7e8d
niravbhatt <niravsbhatt2002@gmail.com>
2024-12-05 01:07:49 +0530

    Added Query Operations for reading from database

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,22 +1,44 @@
+async function products(parent, args, { prisma }, info) {
+  return await prisma.product.findMany();
+}
+
+async function categories(parent, args, { prisma }, info) {
+  const categories = await prisma.category.findMany({
+    include: {
+      products: true, // Ensure we include products
+    },
+  });
+
+  // Make sure that the products field is always an array (even if empty)
+  return categories.map((category) => ({
+    ...category,
+    products: category.products || [], // If no products, return an empty array
+  }));
+}
+
+async function users(parent, args, { prisma }, info) {
+  return await prisma.user.findMany();
+}
+
+async function orders(parent, args, { prisma }, info) {
+  return await prisma.order.findMany();
+}
+
+async function reviews(parent, args, { prisma }, info) {
+  return await prisma.review.findMany();
+}
+
+async function companies(parent, args, { prisma }, info) {
+  return await prisma.company.findMany();
+}
+
 const Query = {
-  products(parent, args, { db }, info) {
-    return db.products;
-  },
-  categories(parent, args, { db }, info) {
-    return db.categories;
-  },
-  users(parent, args, { db }, info) {
-    return db.users;
-  },
-  orders(parent, args, { db }, info) {
-    return db.orders;
-  },
-  reviews(parent, args, { db }, info) {
-    return db.reviews;
-  },
-  companies(parent, args, { db }, info) {
-    return db.companies;
-  },
+  products,
+  categories,
+  users,
+  orders,
+  reviews,
+  companies,
 };
 
 export { Query as default };
