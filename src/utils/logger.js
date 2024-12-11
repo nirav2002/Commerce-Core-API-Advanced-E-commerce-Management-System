65adf3c9a55d4a1d6d87a3d100c88fe5235b9d51
niravbhatt <niravsbhatt2002@gmail.com>
2024-12-11 18:42:37 +0530

    Just added color functionality to logger.js to understand the console output better

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -2,40 +2,57 @@ const fs = require("fs");
 const path = require("path");
 const winston = require("winston");
 
-//Ensure the logs directory exists
+// Ensure the logs directory exists
 const logDir = path.join(__dirname, "logs");
 if (!fs.existsSync(logDir)) {
   fs.mkdirSync(logDir);
 }
 
-//Define a custom log format
+// Define a custom log format with colors
 const logFormat = winston.format.combine(
-  winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }), //Add a timestamp
+  winston.format.colorize(), // Add colors to log levels
+  winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }), // Add a timestamp
   winston.format.printf(function (info) {
-    //Customize how Log messages are displayed
-    return (
-      info.timestamp + " [" + info.level.toUpperCase() + "]: " + info.message
-    );
+    // Customize how Log messages are displayed
+    return `${info.timestamp} [${info.level}]: ${info.message}`;
   })
 );
 
-//Create a Logger instance
+// Create a Logger instance
 const logger = winston.createLogger({
-  level: "info", //Log level (info, error, etc.)
-  format: logFormat, //Using the customer format defined above
+  level: "info", // Log level (info, error, etc.)
+  format: logFormat, // Using the custom format defined above
   transports: [
-    //Writing all logs (info and above) to a general log file
+    // Writing all logs (info and above) to a general log file
     new winston.transports.File({
       filename: path.join(logDir, "application.log"),
+      format: winston.format.combine(
+        winston.format.uncolorize(), // Remove colors for file logs
+        winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+        winston.format.printf(function (info) {
+          return `${
+            info.timestamp
+          } [${info.level.toUpperCase()}]: ${info.message}`;
+        })
+      ),
     }),
 
-    //Write only error logs to a seperate file
+    // Write only error logs to a separate file
     new winston.transports.File({
       filename: path.join(logDir, "error.log"),
       level: "error",
+      format: winston.format.combine(
+        winston.format.uncolorize(), // Remove colors for file logs
+        winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+        winston.format.printf(function (info) {
+          return `${
+            info.timestamp
+          } [${info.level.toUpperCase()}]: ${info.message}`;
+        })
+      ),
     }),
 
-    //Display logs on the console
+    // Display logs on the console with colors
     new winston.transports.Console(),
   ],
 });
