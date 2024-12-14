6ec8592256af0782215d8932ce1cdc519617e586
niravbhatt <niravsbhatt2002@gmail.com>
2024-12-14 16:47:20 +0530

    Just modified the Jest file

--- jest.config.js
+++ jest.config.js
@@ -1,27 +1,16 @@
-/**
- * For a detailed explanation regarding each configuration property, visit:
- * https://jestjs.io/docs/configuration
- */
-
 /** @type {import('jest').Config} */
+
 const config = {
-  // Automatically clear mock calls, instances, contexts, and results before every test
   clearMocks: true,
-
-  // Indicates whether the coverage information should be collected while executing the test
-  collectCoverage: true,
-
-  // The directory where Jest should output its coverage files
+  collectCoverage: false,
   coverageDirectory: "coverage",
-
-  // Indicates which provider should be used to instrument code for coverage
   coverageProvider: "v8",
-
-  // The test environment that will be used for testing
   testEnvironment: "node",
-
-  // The paths to modules that run some code to configure or set up the testing framework before each test
-  setupFiles: ["<rootDir>/jest.setup.js"], // Add this line to use a setup file
+  setupFiles: ["./jest.setup.js"],
+  transform: {
+    "^.+\\.js$": "babel-jest", // Use Babel for transforming JS files
+  },
+  detectOpenHandles: true, // Debug open handles
 };
 
 module.exports = config;
