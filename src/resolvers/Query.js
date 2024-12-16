ece315d7138c09ac5c71c8ab2faa6468381157e7
niravbhatt <niravsbhatt2002@gmail.com>
2024-12-16 11:43:28 +0530

    Just modified the pagination limit for users from 4 to 5

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -76,7 +76,7 @@ async function categories(parent, args, { prisma }, info) {
 
 async function users(parent, args, { prisma }, info) {
   const page = args.page || 1; //Default to page 1
-  const limit = args.limit || 4; //Default to 4 items per page
+  const limit = args.limit || 5; //Default to 5 users per page
   const skip = (page - 1) * limit; //Calculate the number of items to skip
   const searchTerm = args.search || ""; //Default to an empty string if no search term is provided
 
